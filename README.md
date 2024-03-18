d45fbeb11e7d80bef6b8d5477e1c833412affd28
Douglas <douglasrizzom@gmail.com>
2024-03-18 20:43:56 -0300

    First commit, this is wrong

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# pydunn
-Dunn index in Python
+# Dunn index in Python
+
+An implementation of the Dunn index in Python. It sat for ages as a GitHub Gist page, but now I've transferred it to a proper repo so people can report issues more easily.
+
+The original Gist: https://gist.github.com/douglasrizzo/cd7e792ff3a2dcaf27f6
---
+++ pydunn.py
@@ -0,0 +1,150 @@
+from enum import Enum
+
+import numpy as np
+from sklearn.preprocessing import LabelEncoder
+
+
+class DiameterMethod(Enum):
+  MEAN_CLUSTER = 1
+  FARTHEST = 2
+
+
+class ClusterDistanceMethod(Enum):
+  NEAREST = 1
+  FARTHEST = 2
+
+
+def inter_cluster_distances(
+  labels: list[int], distances: np.ndarray, method: ClusterDistanceMethod = ClusterDistanceMethod.NEAREST
+) -> np.ndarray:
+  """Compute inter-cluster distances based on the given labels and distances using the specified method.
+
+  Args:
+    labels (list[int]): The cluster labels for each data point.
+    distances (np.ndarray): The pairwise distances between data points.
+    method (ClusterDistanceMethod, optional): The method to use for calculating inter-cluster distances. Defaults to
+    ClusterDistanceMethod.NEAREST.
+
+  Returns:
+    np.ndarray: The inter-cluster distances matrix.
+  """
+  c_labels = np.unique(labels)
+  n_clusters = len(c_labels)
+  cluster_distances = np.full((n_clusters, n_clusters), float("inf") if method == ClusterDistanceMethod.NEAREST else 0)
+
+  np.fill_diagonal(cluster_distances, 0)
+
+  for i, c1 in enumerate(c_labels):
+    for c2 in c_labels[i + 1 :]:
+      distances_this_cluster = distances[labels == c1].copy()
+      if method == ClusterDistanceMethod.NEAREST:
+        distances_this_cluster[:, labels != c2] = float("inf")
+        cluster_distances[c1, c2] = cluster_distances[c2, c1] = distances_this_cluster.min()
+      else:
+        distances_this_cluster[:, labels != c2] = -float("inf")
+        cluster_distances[c1, c2] = cluster_distances[c2, c1] = distances_this_cluster.max()
+  return cluster_distances
+
+
+def compute_cluster_diameters(
+  labels: list[int], distances: np.ndarray, method: DiameterMethod = DiameterMethod.FARTHEST
+) -> dict[int, float]:
+  """Compute cluster diameters based on the given labels, distances, and diameter computation method.
+
+  Parameters:
+    labels (list[int]): List of cluster labels
+    distances (np.ndarray): 2D array of distances between data points
+    method (DiameterMethod, optional): Method for computing cluster diameters, defaults to DiameterMethod.FARTHEST
+
+  Returns:
+    dict[int, float]: A dictionary containing the computed diameters for each cluster
+  """
+  labels = np.array(labels, dtype=int)
+  if method == DiameterMethod.MEAN_CLUSTER:
+    diameters = {c: distances[labels == c][:, labels == c].sum() for c in np.unique(labels)}
+    for c in np.unique(labels):
+      c_cize = sum(labels == c)
+      diameters[c] /= c_cize * (c_cize - 1) / 2
+
+  elif method == DiameterMethod.FARTHEST:
+    diameters = {c: distances[labels == c][:, labels == c].max() for c in np.unique(labels)}
+
+  return diameters
+
+
+def dunn(
+  labels: list[int],
+  distances: np.ndarray,
+  diameter_method: DiameterMethod = DiameterMethod.FARTHEST,
+  cdist_method: ClusterDistanceMethod = ClusterDistanceMethod.NEAREST,
+) -> float:
+  r"""Compute the Dunn index, the ratio of the minimum inter-cluster distance to the maximum cluster diameter.
+
+  The index is defined as:
+
+  .. math:: D = \min_{i = 1 \ldots n_c; j = i + 1\ldots n_c} \left\lbrace \frac{d \left( c_i,c_j \right)}{\max_{k = 1 \ldots n_c} \left(diam \left(c_k \right) \right)} \right\rbrace
+
+  where :math:`d(c_i,c_j)` represents the distance between clusters :math:`c_i` and :math:`c_j`, and :math:`diam(c_k)`
+  is the diameter of cluster :math:`c_k`.
+
+  - Inter-cluster distance can be defined in many ways, such as the distance between cluster centroids or between their
+  closest elements.
+  - Cluster diameter can be defined as the mean distance between all elements in the cluster, between
+  all elements to the cluster centroid, or as the distance between the two furthest elements.
+
+  The higher the value of the resulting Dunn index, the better the clustering result is considered, since higher values
+  indicate that clusters are compact (small :math:`diam(c_k)`) and far apart (large :math:`d \left( c_i,c_j \right)`).
+
+  Parameters:
+    labels (list[int]): The list of labels for each data point.
+    distances (np.ndarray): The array of distances between data points.
+    diameter_method (DiameterMethod, optional): The method to calculate the cluster diameter. Defaults to
+    DiameterMethod.FARTHEST.
+    cdist_method (ClusterDistanceMethod, optional): The method to calculate the inter-cluster distances. Defaults to
+    ClusterDistanceMethod.NEAREST.
+
+  Returns:
+    float: The ratio of the minimum inter-cluster distance to the maximum cluster diameter.
+
+  References:
+    Kovács, F., Legány, C., & Babos, A. (2005). Cluster validity measurement techniques. 6th International Symposium of
+    Hungarian Researchers on Computational Intelligence.
+  """
+  labels = LabelEncoder().fit_transform(labels)
+  ic_distances = inter_cluster_distances(labels, distances, cdist_method)
+  min_distance = min(ic_distances[ic_distances.nonzero()])
+  max_diameter = max(compute_cluster_diameters(labels, distances, diameter_method))
+  return min_distance / max_diameter
+
+
+if __name__ == "__main__":
+  # from sklearn.cluster import KMeans
+  # from sklearn.datasets import load_iris
+  from sklearn.metrics.pairwise import euclidean_distances
+
+  data = np.array([[0, 0], [0, 1], [1, 0], [1, 1], [10, 10], [10, 14], [14, 10], [14, 14]])
+  labels = [0, 0, 0, 0, 1, 1, 1, 1]
+  distances = euclidean_distances(data)
+
+  for cdist_method in ClusterDistanceMethod:
+    print(cdist_method, "\n", inter_cluster_distances(labels, distances, cdist_method))
+  for diameter_method in DiameterMethod:
+    print(diameter_method, compute_cluster_diameters(labels, distances, diameter_method))
+
+  for diameter_method in DiameterMethod:
+    for cdist_method in ClusterDistanceMethod:
+      dund = dunn(labels, distances, diameter_method, cdist_method)
+      print(diameter_method, cdist_method, dund)
+
+  # data = load_iris()
+  # kmeans = KMeans(n_clusters=3)
+  # iris_labels = data["target"]
+  # iris_features = data["data"]
+  # cluster_labels = kmeans.fit_predict(iris_features)
+  # distances = euclidean_distances(iris_features)
+
+  # for diameter_method in DiameterMethod:
+  #   for cdist_method in ClusterDistanceMethod:
+  #     dund = dunn(iris_labels, distances, diameter_method, cdist_method)
+  #     dunk = dunn(cluster_labels, distances, diameter_method, cdist_method)
+  #     print(diameter_method, cdist_method, dund, dunk)

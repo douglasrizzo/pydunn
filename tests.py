4f7aa056111bcbf91a6130ed1d98fd07725b87a2
Douglas <douglasrizzom@gmail.com>
2024-08-08 15:10:25 -0300

    use logging on tests, compare against validclust

--- tests.py
+++ tests.py
@@ -1,41 +1,59 @@
+import logging
+
 import numpy as np
-from sklearn.cluster import KMeans
+import validclust
+from scipy.spatial.distance import pdist, squareform
 from sklearn.datasets import load_iris
-from sklearn.metrics.pairwise import euclidean_distances
 
 import pydunn
+
+logging.basicConfig(level=logging.INFO, format="[%(asctime)s][%(levelname)s][%(module)s(%(lineno)d)]: %(message)s]")
+logger = logging.getLogger()
 
 
 def test_toy() -> None:
   """Test Dunn index on toy example."""
   data = np.array([[0, 0], [0, 1], [1, 0], [1, 1], [10, 10], [10, 14], [14, 10], [14, 14]])
   labels = [0, 0, 0, 0, 1, 1, 1, 1]
-  distances = euclidean_distances(data)
+  distances = squareform(pdist(data))
 
-  print("#### Distances ####")
   for cdist_method in pydunn.ClusterDistanceMethod:
-    print(cdist_method, "\n", pydunn.inter_cluster_distances(labels, distances, cdist_method))
-  print("\n#### Diameters ####")
+    logger.info(
+      "Distance method %s: %s",
+      cdist_method.name,
+      pydunn.inter_cluster_distances(labels, distances, cdist_method),
+    )
   for diameter_method in pydunn.DiameterMethod:
-    print(diameter_method, pydunn.compute_cluster_diameters(labels, distances, diameter_method))
-
-  print("\n\n#### Dunn ####")
+    logger.info(
+      "Diameter method %s: %s",
+      diameter_method,
+      pydunn.compute_cluster_diameters(labels, distances, diameter_method),
+    )
   for diameter_method in pydunn.DiameterMethod:
     for cdist_method in pydunn.ClusterDistanceMethod:
-      print(diameter_method, cdist_method, pydunn.dunn(labels, distances, diameter_method, cdist_method))
+      logger.info(
+        "%s, %s: %s",
+        diameter_method,
+        cdist_method,
+        pydunn.dunn(labels, distances, diameter_method, cdist_method),
+      )
 
 
 def test_iris() -> None:
-  """Test Dunn index on Iris dataset."""
+  """Test Dunn index on Iris dataset, compare with existing implementation on PyPI."""
   data = load_iris()
-  kmeans = KMeans(n_clusters=3)
   c = data["target"]
   x = data["data"]
-  k = kmeans.fit_predict(x)
-  d = euclidean_distances(x)
+  d = squareform(pdist(x))
+
+  assert validclust.dunn(d, c) == pydunn.dunn(c, d)
 
   for diameter_method in pydunn.DiameterMethod:
     for cdist_method in pydunn.ClusterDistanceMethod:
-      dund = pydunn.dunn(c, d, diameter_method, cdist_method)
-      dunk = pydunn.dunn(k, d, diameter_method, cdist_method)
-      print(diameter_method, cdist_method, dund, dunk)
+      dunn_labels = pydunn.dunn(c, d, diameter_method, cdist_method)
+      logger.info("%s, %s: %s", diameter_method, cdist_method, dunn_labels)
+
+
+if __name__ == "__main__":
+  test_toy()
+  test_iris()
